13b3dca267fbd4b8da18e53d7e024aeb24b12e6e
alfinbudiyanto <alfinbudiyanto26102006@gmail.com>
2024-06-23 23:33:55 +0700

    alfinbudiyanto-4

--- js/script.js
+++ js/script.js
@@ -2,8 +2,8 @@
 const navbarNav = document.querySelector(".navbar-nav");
 // ketika hamburger menu di klik
 document.querySelector("#hamburger-menu").onclick = (e) => {
-  navbarNav.classList.toggle("active");
-  e.preventDefault();
+	navbarNav.classList.toggle("active");
+	e.preventDefault();
 };
 
 // Toggle class active untuk search form
@@ -11,17 +11,17 @@ const searchFrom = document.querySelector(".search-form");
 const searchBox = document.querySelector("#search-box");
 
 document.querySelector("#search-button").onclick = (e) => {
-  searchFrom.classList.toggle("active");
-  searchBox.focus();
-  e.preventDefault();
+	searchFrom.classList.toggle("active");
+	searchBox.focus();
+	e.preventDefault();
 };
 
 // Toggle class active untuk shopping cart
 const shoppingCart = document.querySelector(".shopping-cart");
 
 document.querySelector("#shopping-cart-button").onclick = (e) => {
-  shoppingCart.classList.toggle("active");
-  e.preventDefault();
+	shoppingCart.classList.toggle("active");
+	e.preventDefault();
 };
 
 // klik di luar elemen
@@ -30,17 +30,17 @@ const sb = document.querySelector("#search-button");
 const sc = document.querySelector("#shopping-cart-button");
 
 document.addEventListener("click", function (e) {
-  if (!hm.contains(e.target) && !navbarNav.contains(e.target)) {
-    navbarNav.classList.remove("active");
-  }
+	if (!hm.contains(e.target) && !navbarNav.contains(e.target)) {
+		navbarNav.classList.remove("active");
+	}
 
-  if (!sb.contains(e.target) && !searchFrom.contains(e.target)) {
-    searchFrom.classList.remove("active");
-  }
+	if (!sb.contains(e.target) && !searchFrom.contains(e.target)) {
+		searchFrom.classList.remove("active");
+	}
 
-  if (!sc.contains(e.target) && !shoppingCart.contains(e.target)) {
-    shoppingCart.classList.remove("active");
-  }
+	if (!sc.contains(e.target) && !shoppingCart.contains(e.target)) {
+		shoppingCart.classList.remove("active");
+	}
 });
 
 // Modal Box
@@ -48,30 +48,30 @@ const itemDetailModal = document.querySelector("#item-detail-modal");
 const itemDetailButtons = document.querySelectorAll(".item-detail-button");
 
 itemDetailButtons.forEach((btn) => {
-  btn.onclick = (e) => {
-    itemDetailModal.style.display = "flex";
-    e.preventDefault();
-  };
+	btn.onclick = (e) => {
+		itemDetailModal.style.display = "flex";
+		e.preventDefault();
+	};
 });
 
 // klik tombol close
 document.querySelector(".modal .close-icon").onclick = (e) => {
-  itemDetailModal.style.display = "none";
-  e.preventDefault();
+	itemDetailModal.style.display = "none";
+	e.preventDefault();
 };
 
 // klik di luar modal
 window.onclick = (e) => {
-  if (e.target === itemDetailModal) {
-    itemDetailModal.style.display = "none";
-  }
+	if (e.target === itemDetailModal) {
+		itemDetailModal.style.display = "none";
+	}
 };
 
 // konversi ke Rupiah
 const rupiah = (number) => {
-  return new Intl.NumberFormat("id-ID", {
-    style: "currency",
-    currency: "IDR",
-    minimumFractionDigits: 0,
-  }).format(number);
+	return new Intl.NumberFormat("id-ID", {
+		style: "currency",
+		currency: "IDR",
+		minimumFractionDigits: 0,
+	}).format(number);
 };
